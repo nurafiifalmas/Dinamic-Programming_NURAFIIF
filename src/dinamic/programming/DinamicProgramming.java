3839f312c8a43f02c905c01d25fbbc60e3ef5a80
user <user@LAPTOP-47AR98S0>
2018-09-12 12:56:01 +0700

    initial Commit

---
+++ src/dinamic/programming/DinamicProgramming.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinamic.programming;
+
+/**
+ *
+ * @author user
+ */
+public class DinamicProgramming {
+
+    private static void judul(String identitas) {
+        System.out.println("Identitas : "+identitas);
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1,1,2,3,5,8,13,21, ... dst.");
+    }
+    public static void main(String[] args) {
+        String identitas = "Nurafiif Almas Azhari / X RPL 5 / 33";
+        judul(identitas);
+    
+     
+    }
+    
+}
