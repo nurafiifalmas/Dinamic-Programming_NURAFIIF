9e9cf1281230daf1d45e9159b7b80aded1100117
user <user@LAPTOP-47AR98S0>
2018-09-12 13:23:39 +0700

    Tambah Tampil Hasil

--- src/dinamic/programming/DinamicProgramming.java
+++ src/dinamic/programming/DinamicProgramming.java
@@ -16,17 +16,18 @@ public class DinamicProgramming {
     private static void judul(String identitas) {
         System.out.println("Identitas : "+identitas);
         System.out.println("\nHitung Fibonacci");
-        System.out.println("1,1,2,3,5,8,13,21, ... dst.");
+        System.out.println("1,1,2,3,5,8,13,21, ... dst.\n");
     }
     public static void main(String[] args) {
         String identitas = "Nurafiif Almas Azhari / X RPL 5 / 33";
         judul(identitas);
         int n = input();
        BigInteger hasil = fibo(n);
+       Hasil(n, hasil);
     }
      private static int input() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Bilangan ke- :");
+        System.out.print("Bilangan ke- :");
        int n = input.nextInt();
        return n;
     }
@@ -36,9 +37,12 @@ public class DinamicProgramming {
          hasil [0] = BigInteger.ONE;
          hasil [1] = BigInteger.ONE;
          
-         for (int i =2; i < n;n++){
+         for (int i =2; i < n ; i++){
              hasil[i] = hasil [i - 1].add(hasil[i-2]);
          }
              return hasil[n-1];
     }
+      private static void Hasil (int n, BigInteger hasil){
+          System.out.println("Bilangan Fibonacci ke - "+n+" : "+hasil);
+      }
 }
